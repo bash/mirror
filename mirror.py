5fd5efd19f57d22d5ee0fc6c3b37c4ebe0436848
Tau Gärtli <git@tau.garden>
2024-09-16 18:34:36 +0200

    Sleep, stderr

--- mirror.py
+++ mirror.py
@@ -6,6 +6,8 @@ import os
 from os import path
 from urllib.parse import urlsplit, urlunsplit
 from termcolor import colored
+import sys
+import time
 
 def origin(url: str) -> str:
     parts = urlsplit(url)
@@ -18,14 +20,17 @@ g = Github(auth=auth)
 mirror_dir = os.environ['MIRRORED']
 
 
-for repo in g.get_user().get_repos():
+for i, repo in enumerate(g.get_user().get_repos()):
+    if i % 20 == 0:
+        print(f'{colored('info:', attrs=['bold'])} sleeping for a bit', file=sys.stderr)
+        time.sleep(5)
     clone_dir = path.join(mirror_dir, repo.full_name)
     os.makedirs(clone_dir, exist_ok=True)
     if path.exists(path.join(clone_dir, 'config')):
-        print(f'{colored('info:', attrs=['bold'])} updating {colored(repo.full_name, 'cyan')}')
+        print(f'{colored('info:', attrs=['bold'])} updating {colored(repo.full_name, 'cyan')}', file=sys.stderr)
         check_call(['git', '-C', clone_dir, 'fetch', '--tags'])
     else:
-        print(f'{colored('info:', attrs=['bold'])} cloning {colored(repo.full_name, 'cyan')}')
+        print(f'{colored('info:', attrs=['bold'])} cloning {colored(repo.full_name, 'cyan')}', file=sys.stderr)
         check_call(
             [
                 'git', 'clone', '--mirror',
